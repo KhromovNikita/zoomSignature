0168e7ea20494ebc70031f85e35b4fb1bb2f1623
Nikita Khromov <Nikita.Khromov@onlyoffice.com>
2022-02-03 14:46:49 +0300

    Zoom signature generator

---
+++ main.js
@@ -0,0 +1,44 @@
+const http = require('http')
+const crypto = require('crypto')
+var qs = require('querystring');
+
+function generateSignature(apiKey, apiSecret, meetingNumber, role) {
+	const timestamp = new Date().getTime() - 30000
+	const msg = Buffer.from(apiKey + meetingNumber + timestamp + role).toString('base64')
+	const hash = crypto.createHmac('sha256', apiSecret).update(msg).digest('base64')
+	const signature = Buffer.from(`${apiKey}.${meetingNumber}.${timestamp}.${role}.${hash}`).toString('base64')
+
+	return signature;
+}
+
+ // server
+const hostname = '127.0.0.1'
+const port = process.env.PORT || 8000
+const server = http.createServer((request, response) => {
+	let data, API_KEY, API_SECRET, meet_number, role_id;
+
+	// загрузка данных
+	request.on("data", chunk => {
+		data += chunk;
+		data = qs.parse(data);
+		API_KEY = data["api_key"];
+		API_SECRET = data["api_secret"];
+		meet_number = data["meet_number"];
+		role_id = data["role_id"];
+	});
+
+	// в конце загрузки данных посылаем запрос по targetUrl
+	request.on("end", () => {
+		response.statusCode = 200
+		response.setHeader('Content-Type', 'text/plain')
+		response.setHeader('Access-Control-Allow-Origin', '*');
+		let signature = generateSignature(API_KEY, API_SECRET, meet_number, role_id)
+		response.end(signature);
+	});
+})
+server.listen(port, hostname, () => {
+	console.log(`Server running at http://${hostname}:${port}/`)
+})
+
+
+
